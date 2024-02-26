aa89695bc21baa1b27607727bd2462e1b0107d5b
Florian Schubert <florian.schubert.sg@gmail.com>
2024-02-26 20:28:22 +0100

    fix: auto opens index.html

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ async fn main() -> anyhow::Result<()> {
 
     Ok(HttpServer::new(move || App::new()
         .service(web::calendar::get_events)
-        .service(fs::Files::new("/", "static/")))
+        .service(fs::Files::new("/", "static/").index_file("index.html")))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await?)
