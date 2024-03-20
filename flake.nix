d6d2d16dfc8d33161d7dc4fd4ec60c0a0c3e64c3
Florian Schubert <florian.schubert.sg@gmail.com>
2024-03-20 23:16:11 +0100

    fix: nix build now works with sqlx

--- flake.nix
+++ flake.nix
@@ -51,38 +51,80 @@
           inherit system overlays;
         };
         craneLib = crane.lib.${system};
-
         inherit (pkgs) lib;
 
-        my-crate = craneLib.buildPackage {
-          src = lib.cleanSourceWith {
-            src = ./.; # The original, unfiltered source
-            filter = path: type:
-              (lib.hasSuffix "\.sql" path)
-              || (lib.hasSuffix "\.gitkeep" path)
-              ||
-              # Default filter from crane (allow .rs files)
-              (craneLib.filterCargoSources path type);
-          };
+
+        sqlFilter = path: _type: null != builtins.match ".*sql$" path;
+        sqlOrCargo = path: type: (sqlFilter path type) || (craneLib.filterCargoSources path type);
+
+        src = lib.cleanSourceWith {
+          src = craneLib.path ./.; # The original, unfiltered source
+          filter = sqlOrCargo;
+        };
+
+        # Common arguments can be set here to avoid repeating them later
+        commonArgs = {
+          inherit src;
           strictDeps = true;
+
+          nativeBuildInputs = [
+            pkgs.pkg-config
+          ];
+
+          buildInputs = [
+            # Add additional build inputs here
+            pkgs.openssl
+          ] ++ lib.optionals pkgs.stdenv.isDarwin [
+            # Additional darwin specific inputs can be set here
+            pkgs.libiconv
+            pkgs.darwin.apple_sdk.frameworks.Security
+          ];
+
+          # Additional environment variables can be set directly
+          # MY_CUSTOM_VAR = "some value";
+        };
+
+        # Build *just* the cargo dependencies, so we can reuse
+        # all of that work (e.g. via cachix) when running in CI
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        # Build the actual crate itself, reusing the dependency
+        # artifacts from above.
+        my-crate = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
 
           nativeBuildInputs = with pkgs; [pkg-config];
           buildInputs =
             [
               pkgs.openssl
+              pkgs.sqlx-cli
+              pkgs.postgresql
               # Add additional build inputs here
             ]
             ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
               # Additional darwin specific inputs can be set here
               pkgs.libiconv
             ];
+          
+          # Start postgreSQL and run migrations
+          preBuild = ''
+            ${pkgs.postgresql}/bin/initdb -D ./database
+            sockets=$(mktemp -d)
+            ${pkgs.postgresql}/bin/pg_ctl -D ./database -o "-k $sockets -h \"\"" start
+            # Write the postgresql socket to DATABASE_URL
+            sockets=$(echo $sockets | sed 's/\//%2f/g')
+            export DATABASE_URL="postgresql://$sockets:5432/postgres"
+            echo "DATABASE_URL=$DATABASE_URL"
+            ${pkgs.sqlx-cli}/bin/sqlx migrate run --source ./migrations  --database-url $DATABASE_URL
+          '';
+
           postInstall = ''
             cp -r migrations $out/bin/migrations
           '';
 
           # Additional environment variables can be set directly
           # MY_CUSTOM_VAR = "some value";
-        };
+        });
       in rec {
         checks = {
           inherit my-crate;
@@ -133,6 +175,8 @@
             cargo
             wasm-pack
             cargo-binutils
+            sqlx-cli
+            postgresql
           ];
         };
       }
