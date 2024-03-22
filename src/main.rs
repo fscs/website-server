e07ae82ac92244b6fd09a673e463700792cb9d0f
Florian Schubert <florian.schubert.sg@gmail.com>
2024-03-22 13:23:45 +0100

    recfactor: moved server start to web module

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 mod cache;
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::PathBuf;
+
 use std::str::FromStr;
 
 mod database;
@@ -9,17 +7,10 @@ mod domain;
 mod web;
 
 use crate::database::DatabasePool;
-use actix_files as fs;
-use actix_web::dev::ServiceResponse;
-use actix_web::middleware::{ErrorHandlerResponse, ErrorHandlers};
-use actix_web::web::Data;
-use actix_web::{App, HttpServer};
 use anyhow::anyhow;
 use clap::Parser;
 use lazy_static::lazy_static;
 use log::LevelFilter;
-use reqwest::StatusCode;
-use web::topmanager;
 
 #[derive(Parser)]
 struct Args {
@@ -53,17 +44,7 @@ async fn main() -> anyhow::Result<()> {
     let database = DatabasePool::new(&ARGS.database_url).await?;
     sqlx::migrate!().run(database.pool()).await?;
 
-    Ok(HttpServer::new(move || {
-        App::new()
-            .wrap(ErrorHandlers::new().handler(StatusCode::NOT_FOUND, not_found))
-            .service(web::calendar::service("/api/calendar"))
-            .service(topmanager::service("/api/topmanager"))
-            .service(fs::Files::new("/", dir.clone() + "/static/").index_file("index.html"))
-            .app_data(Data::new(database.clone()))
-    })
-    .bind((ARGS.host.as_str(), ARGS.port))?
-    .run()
-    .await?)
+    web::start_server(dir, database).await
 }
 
 fn get_base_dir() -> anyhow::Result<String> {
@@ -83,27 +64,4 @@ fn get_base_dir() -> anyhow::Result<String> {
             ))?
             .to_string()
     })
-}
-
-fn not_found<B>(
-    res: actix_web::dev::ServiceResponse<B>,
-) -> actix_web::Result<actix_web::middleware::ErrorHandlerResponse<B>> {
-    if res.headers().get("content-type") != Some(&actix_web::http::header::HeaderValue::from_static("text/html")) {
-        return Ok(ErrorHandlerResponse::Response(res.map_into_left_body()));
-    };
-
-    let (req, res) = res.into_parts();
-    let path = PathBuf::from_str(format!("/{}/static/404.html", get_base_dir().unwrap()).as_str()).unwrap();
-    let mut file = File::open(path).unwrap();
-    
-    let mut content = String::new();
-    file.read_to_string(&mut content).unwrap();
-
-    let res = res.set_body(content);
-
-    let res = ServiceResponse::new(req, res)
-        .map_into_boxed_body()
-        .map_into_right_body();
-
-    Ok(ErrorHandlerResponse::Response(res))
 }
--- src/web/mod.rs
+++ src/web/mod.rs
@@ -1,8 +1,18 @@
+use std::fs::File;
 use crate::database::{DatabasePool, DatabaseTransaction};
-use actix_web::dev::Payload;
-use actix_web::{FromRequest, HttpRequest};
+use actix_web::dev::{Payload, ServiceResponse};
+use actix_web::{App, FromRequest, HttpRequest, HttpServer};
 use std::future::Future;
+use std::path::PathBuf;
 use std::pin::Pin;
+use actix_web::middleware::{ErrorHandlerResponse, ErrorHandlers};
+use anyhow::Error;
+use actix_files as fs;
+use actix_web::http::StatusCode;
+use actix_web::web::Data;
+use std::str::FromStr;
+use std::io::Read;
+use crate::{ARGS, get_base_dir, web};
 
 pub(crate) mod calendar;
 pub(crate) mod topmanager;
@@ -32,4 +42,41 @@ impl FromRequest for DatabaseTransaction<'static> {
             }
         })
     }
+}
+
+pub async fn start_server(dir: String, database: DatabasePool) -> Result<(), Error> {
+    Ok(HttpServer::new(move || {
+        App::new()
+            .wrap(ErrorHandlers::new().handler(StatusCode::NOT_FOUND, not_found))
+            .service(web::calendar::service("/api/calendar"))
+            .service(topmanager::service("/api/topmanager"))
+            .service(fs::Files::new("/", dir.clone() + "/static/").index_file("index.html"))
+            .app_data(Data::new(database.clone()))
+    })
+        .bind((ARGS.host.as_str(), ARGS.port))?
+        .run()
+        .await?)
+}
+
+fn not_found<B>(
+    res: actix_web::dev::ServiceResponse<B>,
+) -> actix_web::Result<actix_web::middleware::ErrorHandlerResponse<B>> {
+    if res.headers().get("content-type") != Some(&actix_web::http::header::HeaderValue::from_static("text/html")) {
+        return Ok(ErrorHandlerResponse::Response(res.map_into_left_body()));
+    };
+
+    let (req, res) = res.into_parts();
+    let path = PathBuf::from_str(format!("/{}/static/404.html", get_base_dir().unwrap()).as_str()).unwrap();
+    let mut file = File::open(path).unwrap();
+
+    let mut content = String::new();
+    file.read_to_string(&mut content).unwrap();
+
+    let res = res.set_body(content);
+
+    let res = ServiceResponse::new(req, res)
+        .map_into_boxed_body()
+        .map_into_right_body();
+
+    Ok(ErrorHandlerResponse::Response(res))
 }
