5cf5429532c09da13a7a83e9dfedd004f10e37ec
Florian Schubert <florian.schubert.sg@gmail.com>
2024-04-22 13:14:00 +0200

    Added Tests for Cache

--- src/cache.rs
+++ src/cache.rs
@@ -30,6 +30,10 @@ struct DataLastUpdate<T> {
 }
 
 impl<T: Sync> TimedCache<T> {
+
+    /// Create a TimedCache that generates a Value from the given Function.
+    /// The Function is normally not `pure`, it is expected, that the Output can change.
+    /// The Duration should be a Time in which, the Output is expected to not change.
     pub(crate) fn with_generator<FN: Fn() -> Pin<Box<dyn Future<Output = T>>> + 'static + Sync>(
         generator: FN,
         duration: Duration,
@@ -41,6 +45,23 @@ impl<T: Sync> TimedCache<T> {
         }
     }
 
+    /// Get the Value of the TimedCache. 
+    ///
+    /// Executes the generator function, if the last time it ran is more than Duration ago.
+    ///
+    ///
+    /// ## Usage 
+    /// ```rust
+    /// # tokio_test::block_on(async {
+    /// lazy_static! {
+    ///   static ref CACHE: TimedCache<u64> TimedCache::with_generator(
+    ///     Box::pin(|| async {0}),
+    ///     std::time::Duration::from_secs(60));
+    /// }
+    ///
+    /// assert_eq!(CACHE.get.await, 0)
+    /// # })
+    /// ```
     pub(crate) async fn get(&self) -> impl Deref<Target = T> + '_ {
         let data = self.data_last_updated.read().await;
         if (data)
@@ -86,7 +107,7 @@ impl<T: Sync, E: Sync> TimedCache<Result<T,E>> {
             if (*write).is_none()
                 || (*write)
                     .iter()
-                    .any(|d| d.last_updated + self.duration < SystemTime::now())
+                    .any(|d| d.last_updated + self.duration < SystemTime::now() || d.data.is_err())
             {
                 let data = (*self.generator)().await;
 
@@ -98,5 +119,49 @@ impl<T: Sync, E: Sync> TimedCache<Result<T,E>> {
             let data = RwLockWriteGuard::downgrade(write);
             ReadWrapper(data)
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::sync::Arc;
+
+    use std::sync::Mutex;
+    use lazy_static::lazy_static;
+
+    use crate::cache::TimedCache;
+
+
+    #[tokio::test]
+    async fn test_cache() {
+        lazy_static! {
+          static ref CACHE: TimedCache<u64> = TimedCache::with_generator(
+            || Box::pin(async {0}),
+            std::time::Duration::from_secs(60));
+        }
+        
+        assert_eq!(*CACHE.get().await, 0)
+    }
+
+    #[tokio::test]
+    async fn test_retry() {
+        let x = Arc::new(Mutex::new(0));
+        let a = x.clone();
+
+        let cache = TimedCache::with_generator(move || {
+            let x = a.clone();
+            Box::pin(async move {
+                let mut x = x.lock().unwrap();
+                *x = *x + 1;
+            if *x == 1 {
+                Err(1)
+            } else {
+                Ok(2)
+            }
+        })}, std::time::Duration::from_secs(60));
+
+        assert!(*cache.try_get().await == Err(1));
+        assert!(*cache.try_get().await == Ok(2))
+
     }
 }
