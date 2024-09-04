60680897d4534bfe7ff18e9b7c49ea15cc3924e0
Felix Nilles <felix@dienilles.de>
2024-09-04 23:21:08 +0200

    refactor: added Documentation to person.rs

--- src/web/persons.rs
+++ src/web/persons.rs
@@ -16,13 +16,26 @@ pub(crate) fn service(path: &'static str) -> Scope {
         .service(get_persons_by_role)
 }
 
-#[utoipa::path()]
+#[utoipa::path(
+    path = "/api/persons/",
+    responses(
+        (status = 200, description = "Success", body = Vec<Person>),
+        (status = 500, description = "Internal Server Error"),
+    )
+)]
 #[get("/")]
 async fn get_persons(mut transaction: DatabaseTransaction<'_>) -> impl Responder {
     RestStatus::ok_from_result(transaction.persons().await)
 }
 
-#[utoipa::path()]
+#[utoipa::path(
+    path = "/api/persons/{id}/",
+    responses(
+        (status = 200, description = "Success", body = Person),
+        (status = 404, description = "Not Found"),
+        (status = 500, description = "Internal Server Error"),
+    )
+)]
 #[get("/{id}/")]
 async fn get_person_by_id(
     id: Path<Uuid>,
@@ -31,7 +44,14 @@ async fn get_person_by_id(
     RestStatus::ok_from_result(transaction.person_by_id(*id).await)
 }
 
-#[utoipa::path()]
+#[utoipa::path(
+    path = "/api/persons/{id}/",
+    responses(
+        (status = 200, description = "Success"),
+        (status = 404, description = "Not Found"),
+        (status = 500, description = "Internal Server Error"),
+    )
+)]
 #[delete("/{id}/")]
 async fn delete_person_by_id(
     id: Path<Uuid>,
@@ -40,7 +60,14 @@ async fn delete_person_by_id(
     RestStatus::ok_from_result(transaction.delete_person(*id).await)
 }
 
-#[utoipa::path()]
+#[utoipa::path(
+    path = "/api/persons/{id}/",
+    responses(
+        (status = 200, description = "Success", body = Person),
+        (status = 404, description = "Not Found"),
+        (status = 500, description = "Internal Server Error"),
+    )
+)]
 #[patch("/{id}/")]
 async fn patch_person(
     id: Path<Uuid>,
@@ -55,8 +82,15 @@ struct PersonsByRoleParams {
     role: String,
 }
 
-#[utoipa::path()]
-#[get("/by-role")]
+#[utoipa::path(
+    path = "/api/persons/by-role/",
+    responses(
+        (status = 200, description = "Success", body = Vec<Person>),
+        (status = 404, description = "Not Found"),
+        (status = 500, description = "Internal Server Error"),
+    )
+)]
+#[get("/by-role/")]
 async fn get_persons_by_role(
     param: Query<PersonsByRoleParams>,
     mut transaction: DatabaseTransaction<'_>,
@@ -64,32 +98,48 @@ async fn get_persons_by_role(
     RestStatus::ok_from_result(transaction.persons_with_role(&param.role).await)
 }
 
-#[derive(Deserialize)]
-struct AddRoleToPersonParams {
-    role: String,
-}
+// TODO: person/{id}/roles/
+//
+// #[derive(Deserialize)]
+// struct AddRoleToPersonParams {
+//     role: String,
+// }
 
-#[utoipa::path()]
-#[post("/{id}/roles/")]
-async fn add_role_to_person(
-    id: Path<Uuid>,
-    param: web::Json<AddRoleToPersonParams>,
-    mut transaction: DatabaseTransaction<'_>,
-) -> impl Responder {
-    RestStatus::created_from_result(transaction.assign_role_to_person(*id, &param.role).await)
-}
+// #[utoipa::path(
+//     path = "/api/persons/{id}/roles/",
+//     responses(
+//         (status = 200, description = "Success", body = Vec<Role>),
+//         (status = 404, description = "Not Found"),
+//         (status = 500, description = "Internal Server Error"),
+//     )
+// )]
+// #[post("/{id}/roles/")]
+// async fn add_role_to_person(
+//     id: Path<Uuid>,
+//     param: web::Json<AddRoleToPersonParams>,
+//     mut transaction: DatabaseTransaction<'_>,
+// ) -> impl Responder {
+//     RestStatus::created_from_result(transaction.assign_role_to_person(*id, &param.role).await)
+// }
 
-#[derive(Deserialize)]
-struct DeleteRoleFromPersonParams {
-    role: String,
-}
+// #[derive(Deserialize)]
+// struct DeleteRoleFromPersonParams {
+//     role: String,
+// }
 
-#[utoipa::path()]
-#[delete("/{id}/roles/")]
-async fn delete_role_from_person(
-    id: Path<Uuid>,
-    param: web::Json<DeleteRoleFromPersonParams>,
-    mut transaction: DatabaseTransaction<'_>,
-) -> impl Responder {
-    RestStatus::created_from_result(transaction.revoke_role_from_person(*id, &param.role).await)
-}
+// #[utoipa::path(
+//     path = "/api/persons/{id}/roles/",
+//     responses(
+//         (status = 200, description = "Success"),
+//         (status = 404, description = "Not Found"),
+//         (status = 500, description = "Internal Server Error"),
+//     )
+// )]
+// #[delete("/{id}/roles/")]
+// async fn delete_role_from_person(
+//     id: Path<Uuid>,
+//     param: web::Json<DeleteRoleFromPersonParams>,
+//     mut transaction: DatabaseTransaction<'_>,
+// ) -> impl Responder {
+//     RestStatus::created_from_result(transaction.revoke_role_from_person(*id, &param.role).await)
+// }
