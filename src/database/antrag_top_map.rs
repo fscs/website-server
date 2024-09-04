da6457e521503fe5d98b24817ff68aa16549c122
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2024-09-04 16:11:47 +0200

    refactor: implement database antrag top mapping

--- src/database/antrag_top_map.rs
+++ src/database/antrag_top_map.rs
@@ -2,26 +2,45 @@ use anyhow::Result;
 use sqlx::PgConnection;
 use uuid::Uuid;
 
-use crate::domain::{Antrag, AntragTopMapRepo, AntragTopMapping};
+use crate::domain::{Antrag, AntragData, AntragTopMapRepo, AntragTopMapping};
 
 impl AntragTopMapRepo for PgConnection {
     async fn anträge_by_top(&mut self, top_id: Uuid) -> Result<Vec<Antrag>> {
-        todo!()
-        // let result = sqlx::query_as!(
-        //     Antrag,
-        //     r#"
-        //         SELECT anträge.id, anträge.antragstext, anträge.begründung, anträge.titel 
-        //         FROM anträge
-        //         JOIN antragstop 
-        //         ON anträge.id = antragstop.antrag_id 
-        //         WHERE antragstop.top_id = $1
-        //     "#,
-        //     top_id
-        // )
-        // .fetch_all(self)
-        // .await?;
+        let records = sqlx::query!(
+            r#"
+                SELECT 
+                    anträge.id, 
+                    anträge.antragstext, 
+                    anträge.begründung, 
+                    anträge.titel, 
+                    ARRAY_AGG(antragsstellende.person_id) AS creators
+                FROM anträge
+                JOIN antragstop 
+                ON anträge.id = antragstop.antrag_id 
+                JOIN antragsstellende
+                ON anträge.id = antragsstellende.antrags_id
+                WHERE antragstop.top_id = $1
+                GROUP BY anträge.id
+            "#,
+            top_id
+        )
+        .fetch_all(self)
+        .await?;
 
-        // Ok(result)
+        let result = records
+            .iter()
+            .map(|r| Antrag {
+                data: AntragData {
+                    id: r.id.clone(),
+                    antragstext: r.antragstext.clone(),
+                    begründung: r.begründung.clone(),
+                    titel: r.titel.clone(),
+                },
+                creators: r.creators.clone().unwrap_or_default(),
+            })
+            .collect();
+
+        Ok(result)
     }
 
     async fn attach_antrag_to_top(
@@ -55,6 +74,84 @@ impl AntragTopMapRepo for PgConnection {
         )
         .execute(self)
         .await?;
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use anyhow::Result;
+    use sqlx::PgPool;
+    use uuid::Uuid;
+
+    use crate::domain::AntragTopMapRepo;
+
+    #[sqlx::test(fixtures(
+        "gimme_persons",
+        "gimme_sitzungen",
+        "gimme_tops",
+        "gimme_antraege",
+        "gimme_antrag_mappings"
+    ))]
+    async fn anträge_by_top(pool: PgPool) -> Result<()> {
+        let mut conn = pool.acquire().await?;
+
+        let top_id = Uuid::parse_str("fd6b67df-60f2-453a-9ffc-93514c5ccdb1").unwrap();
+
+        let anträge = conn.anträge_by_top(top_id).await?;
+
+        let antrag_id = Uuid::parse_str("46148231-87b0-4486-8043-c55038178518").unwrap();
+
+        assert_eq!(anträge.len(), 1);
+
+        assert!(anträge.iter().any(|e| e.data.id == antrag_id));
+
+        Ok(())
+    }
+    
+    #[sqlx::test(fixtures(
+        "gimme_persons",
+        "gimme_sitzungen",
+        "gimme_tops",
+        "gimme_antraege",
+        "gimme_antrag_mappings"
+    ))]
+    async fn attach_antrag_to_top(pool: PgPool) -> Result<()> {
+        let mut conn = pool.acquire().await?;
+
+        let top_id = Uuid::parse_str("fd6b67df-60f2-453a-9ffc-93514c5ccdb1").unwrap();
+        let antrag_id = Uuid::parse_str("5c51d5c0-3943-4695-844d-4c47da854fac").unwrap();
+
+        conn.attach_antrag_to_top(antrag_id, top_id).await?;
+
+        let anträge = conn.anträge_by_top(top_id).await?;
+
+        assert_eq!(anträge.len(), 2);
+
+        assert!(anträge.iter().any(|e| e.data.id == antrag_id));
+
+        Ok(())
+    }
+    
+    #[sqlx::test(fixtures(
+        "gimme_persons",
+        "gimme_sitzungen",
+        "gimme_tops",
+        "gimme_antraege",
+        "gimme_antrag_mappings"
+    ))]
+    async fn detach_antrag_from_top(pool: PgPool) -> Result<()> {
+        let mut conn = pool.acquire().await?;
+
+        let top_id = Uuid::parse_str("fd6b67df-60f2-453a-9ffc-93514c5ccdb1").unwrap();
+        let antrag_id = Uuid::parse_str("46148231-87b0-4486-8043-c55038178518").unwrap();
+
+        conn.detach_antrag_from_top(antrag_id, top_id).await?;
+
+        let anträge = conn.anträge_by_top(top_id).await?;
+
+        assert!(anträge.is_empty());
 
         Ok(())
     }
---
+++ src/database/fixtures/gimme_antrag_mappings.sql
@@ -0,0 +1,3 @@
+INSERT INTO antragstop (antrag_id, top_id) 
+    VALUES
+        ('46148231-87b0-4486-8043-c55038178518', 'fd6b67df-60f2-453a-9ffc-93514c5ccdb1');
