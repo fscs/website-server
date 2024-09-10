c677dcce985063ac821de05faa46ff96c7971784
Felix Nilles <felix@dienilles.de>
2024-09-10 14:52:08 +0200

    lit

--- migrations/20_validate_roles.sql
+++ migrations/20_validate_roles.sql
@@ -4,5 +4,8 @@ create table if not exists roles (
     name text not null unique
 );
 
+insert into roles (name)
+    values ('Rat');
+
 alter table rolemapping
     add foreign key(rolle) references roles(name);
