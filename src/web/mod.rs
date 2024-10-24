1133b9d678c2d91e6aa09172ea20414d641357e2
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2024-10-24 21:25:17 +0200

    fix: use async calls for file access

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -1,15 +1,14 @@
 use std::future::Future;
-use std::path::{Component, PathBuf};
+use std::path::{Component, Path, PathBuf};
 use std::pin::Pin;
 
 use actix_cors::Cors;
 use actix_files::NamedFile;
+use actix_http::StatusCode;
 use actix_web::body::BoxBody;
-use actix_web::dev::{Payload, ServiceResponse};
-use actix_web::error::ErrorNotFound;
+use actix_web::dev::Payload;
 use actix_web::http::header::{CacheControl, CacheDirective};
-use actix_web::http::StatusCode;
-use actix_web::middleware::{ErrorHandlerResponse, ErrorHandlers, Logger};
+use actix_web::middleware::Logger;
 use actix_web::web::{scope, Data};
 use actix_web::{
     get, App, FromRequest, HttpRequest, HttpResponse, HttpServer, Responder, ResponseError,
@@ -163,14 +162,8 @@ pub async fn start_server(database: DatabasePool) -> Result<(), Error> {
     Ok(())
 }
 
-#[get(
-    "/{filename:.*}",
-    wrap = "ErrorHandlers::new().handler(StatusCode::NOT_FOUND, file_not_found)"
-)]
-async fn serve_files(
-    req: HttpRequest,
-    user: Option<User>,
-) -> Result<impl Responder, actix_web::Error> {
+#[get("/{filename:.*}")]
+async fn serve_files(req: HttpRequest, user: Option<User>) -> HttpResponse<BoxBody> {
     // decide what the user gets to see
     let base_dir = match user {
         Some(user) => match user.is_rat() {
@@ -185,7 +178,7 @@ async fn serve_files(
     // validate that the sub_path doesnt go backwards
     for component in sub_path.components() {
         if matches!(component, Component::ParentDir | Component::Prefix(_)) {
-            return Err(ErrorNotFound("not found"));
+            return err_not_found(base_dir, req).await;
         }
     }
 
@@ -196,8 +189,8 @@ async fn serve_files(
         path
     };
 
-    let Ok(file) = NamedFile::open(actual_path.as_path()) else {
-        return Err(ErrorNotFound("not found"));
+    let Ok(file) = NamedFile::open_async(actual_path.as_path()).await else {
+        return err_not_found(base_dir, req).await;
     };
 
     // configure headers for cache control
@@ -206,11 +199,11 @@ async fn serve_files(
     // so those can be cached
     let must_revalidate = *file.content_type() == mime::TEXT_HTML;
 
-    // we dont want to set Last-Modified on responses. since we're nix-people, the date will always
-    // be 1970-01-01 which is kind of unnessecary to set
+    // we dont want to set Last-Modified on responses. since the content will live in the nix store
+    // anyway and the date will always be 1970-01-01 which is kind of unnessecary to set
     //
     // ETag should only be set if we want the browser to revalidate
-    let res = if must_revalidate {
+    if must_revalidate {
         file.use_last_modified(false)
             .customize()
             .insert_header(CacheControl(vec![
@@ -219,6 +212,7 @@ async fn serve_files(
                 CacheDirective::MaxAge(0),
             ]))
             .respond_to(&req)
+            .map_into_boxed_body()
     } else {
         file.use_last_modified(false)
             .use_etag(false)
@@ -228,30 +222,20 @@ async fn serve_files(
                 CacheDirective::MaxAge(31_536_000),
             ]))
             .respond_to(&req)
-    };
-
-    Ok(res)
+            .map_into_boxed_body()
+    }
 }
 
-fn file_not_found(
-    srv_res: ServiceResponse<BoxBody>,
-) -> actix_web::Result<ErrorHandlerResponse<BoxBody>> {
-    let req = srv_res.request();
-    let path = CONTENT_DIR.public.join("de/404.html");
+async fn err_not_found(base_dir: &Path, req: HttpRequest) -> HttpResponse<BoxBody> {
+    let path = base_dir.join("de").join("404.html");
 
-    let file = NamedFile::open(path).unwrap();
-
-    let http_res = file
-        .use_last_modified(false)
-        .customize()
-        .insert_header(CacheControl(vec![
-            CacheDirective::Private,
-            CacheDirective::MustRevalidate,
-            CacheDirective::MaxAge(0),
-        ]))
-        .respond_to(req);
-
-    Ok(ErrorHandlerResponse::Response(
-        srv_res.into_response(http_res),
-    ))
+    NamedFile::open_async(path)
+        .await
+        .map(|f| {
+            f.customize()
+                .with_status(StatusCode::NOT_FOUND)
+                .respond_to(&req)
+                .map_into_boxed_body()
+        })
+        .unwrap_or_else(|_| HttpResponse::NotFound().body("<h1>Not found</h1>"))
 }
