d47295d4a510723ec06e430891823614015b214a
Felix Nilles <felix@dienilles.de>
2025-02-17 20:21:29 +0100

    fix: images

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -146,6 +146,7 @@ pub async fn start_server(database: DatabasePool) -> Result<(), Error> {
                     || path.starts_with("/js")
                     || path.starts_with("/favicon.ico")
                     || path.starts_with("/css")
+                    || path.starts_with("/images")
                     || path.starts_with("/scss"))
                 {
                     let new_path = format!("/de{}", path);
