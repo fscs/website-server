6b341b096ed788ee9a58411257de6e80f713a914
0x5a4 <1444@wienstroer.net>
2025-03-18 15:42:44 +0100

    fix: migration pfusch

--- migrations/28_combine_user_full_name.sql
+++ migrations/28_combine_user_full_name.sql
@@ -2,7 +2,7 @@ alter table person
     rename last_name to full_name;
 
 update person set 
-    full_name = first_name || full_name;
+    full_name = first_name || ' ' || full_name;
 
 alter table person
     drop column first_name;
